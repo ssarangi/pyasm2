3840ee8c0d116a638ef28591eca0000fbb6355a0
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-01-10 19:03:02 -0800

    Changed the name :)

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ jittermodule = Extension('jitter',
 
 setup (name = 'PackageName',
        version = '1.0',
-       description = 'This is the Jitter Module for DragonPy',
+       description = 'This is the Jitter Module for Pyasm2',
        ext_modules = [jittermodule])
